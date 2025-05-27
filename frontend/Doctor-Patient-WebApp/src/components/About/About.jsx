9644cb7122b682ad993d023b0b17613e74ca1828
krish-dotcom518 <krishikavenkatesan55@gmail.com>
2025-05-27 22:58:22 +0530

    update

--- frontend/Doctor-Patient-WebApp/src/components/About/About.jsx
+++ frontend/Doctor-Patient-WebApp/src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import aboutImg from '/src/assets/images/about.png';
 import aboutCarding from '/src/assets/images/about-card.png';
-
+import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <section>
@@ -17,7 +17,13 @@ const About = () => {
               <img src={aboutCarding} alt="About Card" className='w-full' />
             </div>
           </div>
-
+          <div className='w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2'>
+            <h2 className='heading'>🌟Proud to be one of the nation's best!🏆✨Delivering excellence, innovation, and care every step of the way!🚀</h2>
+            <p className='text__para'>India has recognized MediCare as a top healthcare provider!🏆✨With world-class doctors, cutting-edge facilities, and a commitment to excellence, it sets new standards in patient care!🏥🩺🚀</p>
+            <Link to='/'>
+            <button className='btn'>Learn More</button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
