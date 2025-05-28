bc5772bcb4d28e0fc8a2bd97377726c70aba0ae5
krish-dotcom518 <krishikavenkatesan55@gmail.com>
2025-05-28 09:08:54 +0530

    update

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app= express()
+const port = process.env.PORT || 8000
+
+const corsOptions={
+    origin:true
+}
+
+app.get('/', (req,res)=>{
+    res.send('Api is Working')
+})
+
+
+app.listen(port, ()=>{
+    console.log("Server is Running on port" + port)
+})
